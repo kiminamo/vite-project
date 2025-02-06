6e53b0b68edb2f9dc14cb452667ba9bf2517f1a4
kiminamo <manalotokimm@gmail.com>
2025-02-06 13:59:32 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1> Kimberly G. Manaloto </h1>
+      <div id="rectangle">CPEITEL</div>
+    </div>
+  )
+}
+
+export default App
